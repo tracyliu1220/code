6a7e8c57885bca86db1764495eb1759a9d88bec0
marmot0814 <32301085+marmot0814@users.noreply.github.com>
2021-02-05 20:24:08 +0800

    修改值域並加大測資答案 (#12)
    * 修改值域並加大測資答案
    
    * 增加極端測資
    
    * Update 13.ans

--- apcs/11001/p4_testcases/gen.cpp
+++ apcs/11001/p4_testcases/gen.cpp
@@ -3,12 +3,27 @@ using namespace std;
 
 void gen(int n, int v_upper) {
     cout << n << '\n';
+    vector<int> X, Y;
+    for (int i = 0 ; i < n ; i++)
+        X.push_back(rand() % v_upper + 1),
+        Y.push_back(rand() % v_upper + 1);
+    sort(X.begin(), X.end());
+    sort(Y.begin(), Y.end());
+    int t = n / 20;
+    while (t--) {
+        int i = rand() % n;
+        int j = rand() % n;
+        swap(Y[i], Y[j]);
+    }
     set<pair<int, int>> ps;
+    for (int i = 0 ; i < n ; i++)
+        ps.insert({X[i], Y[i]});
     while (ps.size() != n) {
         int x = rand() % v_upper + 1;
         int y = rand() % v_upper + 1;
         ps.insert({x, y});
     }
+    assert(ps.size() == n);
     vector<pair<int, int>> ps_vec;
     for (auto &v : ps)
         ps_vec.push_back(v);
@@ -19,23 +34,17 @@ void gen(int n, int v_upper) {
 
 void testcase1() {
     int n = rand() % 100 + 1;
-    if (rand() % 5 >= 1)
-        n = 100;
     int v_upper = 100;
     gen(n, v_upper);
 }
 void testcase2() {
     int n = rand() % 1000 + 1;
-    if (rand() % 5 >= 1)
-        n = 1000;
-    int v_upper = 1e8;
+    int v_upper = 1e7;
     gen(n, v_upper);
 }
 void testcase3() {
     int n = rand() % 200000 + 1;
-    if (rand() % 5 >= 1)
-        n = 200000;
-    int v_upper = 1e8;
+    int v_upper = 1e7;
     gen(n, v_upper);
 }
 
